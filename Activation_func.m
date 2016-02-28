ae8fed2d5d3fb2dc06aabaf6389f48a65d50e877
manageryzy <manageryzy@gmail.com>
2016-02-28 14:34:32 +0800

    init. this is still cpu version

---
+++ Activation_func.m
@@ -0,0 +1,12 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%   Please see readme.md   %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% Activation Function
+function fx = Activation_func(x, unipolarBipolarSelector)
+    if (unipolarBipolarSelector == 0)
+        fx = 1./(1 + exp(-x)); %Binary
+    else
+        fx = -1 + 2./(1 + exp(-x)); %Bipolar
+    end
+end
---
+++ EvaluateNetwork.m
@@ -0,0 +1,20 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%   Please see readme.md   %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function outputs = EvaluateNetwork(Sample, NodesActivations, Weights, unipolarBipolarSelector)
+
+nbrOfLayers = length(NodesActivations);
+
+NodesActivations{1} = Sample;
+for Layer = 2:nbrOfLayers
+    NodesActivations{Layer} = NodesActivations{Layer-1}*Weights{Layer-1};
+    NodesActivations{Layer} = Activation_func(NodesActivations{Layer}, unipolarBipolarSelector);
+    if (Layer ~= nbrOfLayers) %Because bias nodes don't have weights connected to previous layer
+        NodesActivations{Layer}(1) = 1;
+    end
+end
+
+outputs = NodesActivations{end};
+
+end
